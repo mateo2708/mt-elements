3fa86c1d10b6b76df7c60c23d65ab17ac6d0620f
Mateo Tafur <mateotafurg@gmail.com>
2020-05-22 20:07:44 -0500

    Updates

---
+++ precache-manifest.c3865aa2918acfd11d4753226b82f1c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a813acc814a34ba5bbbe9e84f717a795",
+    "url": "/mt-elements/index.html"
+  },
+  {
+    "revision": "86337bcee1df91b01fc7",
+    "url": "/mt-elements/static/css/main.6f9e6625.chunk.css"
+  },
+  {
+    "revision": "0f8bae8110863cd74cdc",
+    "url": "/mt-elements/static/js/2.5779ad8b.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/mt-elements/static/js/2.5779ad8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86337bcee1df91b01fc7",
+    "url": "/mt-elements/static/js/main.dc3fbb07.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "/mt-elements/static/js/main.dc3fbb07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d43a3bf0ba5d2ff0fcda",
+    "url": "/mt-elements/static/js/runtime-main.0585ea01.js"
+  },
+  {
+    "revision": "da0e3503c102e021200dc4dd325a0855",
+    "url": "/mt-elements/static/media/LogoMTElements.da0e3503.svg"
+  }
+]);
